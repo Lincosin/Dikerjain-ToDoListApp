ed7e89cf95b17dbc81326fc19b2ac3eb113a371c
Lincosin <willynugraha129@gmail.com>
2025-12-29 10:56:10 +0800

    Memperbaiki routing

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['user']) && $page !== 'user') {
 // Routing ke controller
 switch ($page) {
     case 'tasks':
-        require_once __DIR__ . '/controllers/TaskController.php';
+        require_once __DIR__ . '/controllers/taskcontroller.php';
         $controller = new TaskController($pdo);
         break;
     case 'calendar':
@@ -22,7 +22,7 @@ switch ($page) {
         $controller = new CalendarController($pdo);
         break;
     case 'settings':
-        require_once __DIR__ . '/controllers/SettingController.php';
+        require_once __DIR__ . '/controllers/settingcontroller.php';
         $controller = new SettingController($pdo);
         break;
     case 'user':
