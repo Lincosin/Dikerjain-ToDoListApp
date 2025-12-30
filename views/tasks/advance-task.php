1ec1ecd938521a6566a66bc640ac2cce92d55d44
nuryanaputra19-stack <nuryanaputra19@gmail.com>
2025-12-30 14:12:14 +0800

    menambahkan struktur halaman untuk fitur Advance Task

--- views/tasks/advance-task.php
+++ views/tasks/advance-task.php
@@ -6,6 +6,26 @@
     <title>Advance Task</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="main-container">
+            <header style="text-align: center; font-size: 24px; font-weight: bold;">
+                Advance Task
+            </header>
+            <div class="SemuaInput">
+                <form action="" method="post">
+                    <input type="text" name="title" id="" required placeholder="Judul Tugas">
+                    <input id="inputdescripsi" type="text" name="description" id="" required placeholder="Keterangan">
+                    <input type="date" name="date" id="" required>
+
+                    <div class="row">
+                        <a href="homepage.php">
+                            <button class="tombolkeluar" type="button">Batal</button>
+                        </a>
+                        <button class="tombolmasuk" type="submit">Simpan</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
