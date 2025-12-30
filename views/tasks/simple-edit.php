8778d949ee1dfbda89f9a65570593ccc8b7a229c
nuryanaputra19-stack <nuryanaputra19@gmail.com>
2025-12-30 14:36:49 +0800

    menambahkan struktur halaman untuk fitur Simple Edit Task

--- views/tasks/simple-edit.php
+++ views/tasks/simple-edit.php
@@ -6,6 +6,24 @@
     <title>Simple Edit Task</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="card">
+            <div class="cardbody">
+                <h2 style="text-align: center; margin-bottom: 20px;">Edit Task</h2>
+                <form action="" method="post">
+                    <div class="cardinput">
+                        <input class="inputtask" type="text" name="title" id="" required>
+                        <input class="inputtask" type="date" name="date" id="" required>
+                    </div>
+                    <div class="tombol">
+                        <a href="homepage.php">
+                            <button class="tombolkeluar" type="button">Batal</button>
+                        </a>
+                        <button class="tombolmasuk" type="submit">Update</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
