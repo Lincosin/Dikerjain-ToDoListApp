81f0516789c0d9e9d4b75cd4f9a940cb63558df5
Lincosin <willynugraha129@gmail.com>
2025-12-30 19:40:29 +0800

    Update routing halaman task

--- controllers/taskcontroller.php
+++ controllers/taskcontroller.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/../config.php';
 require_once __DIR__ . '/../models/taskmodel.php';
 
 class TaskController {
@@ -9,7 +10,23 @@ class TaskController {
     }
 
     public function index() {
-        // $tasks = Task::all();
+        $taskModel = new TaskModel($this->pdo);
+        $tasks = $taskModel->getAll();
         require_once __DIR__ . '/../views/tasks/index.php';
+    }
+
+    public function create() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $title = $_POST['title'];
+            $description = $_POST['description'];
+
+            $taskModel = new TaskModel($this->pdo);
+            $taskModel->createTask($title, $description);
+
+            header('Location: /tasks');
+            exit();
+        }
+
+        require_once __DIR__ . '/../views/tasks/create.php';
     }
 }
