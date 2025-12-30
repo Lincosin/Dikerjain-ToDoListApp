4e7b747f7634e21b77dc5aa1200576cdd8558dab
nuryanaputra19-stack <nuryanaputra19@gmail.com>
2025-12-30 13:04:12 +0800

    menambahkan halaman popup

--- views/tasks/homepage.php
+++ views/tasks/homepage.php
@@ -24,6 +24,30 @@
             <!-- Tombol buat Task Simple -->
             <button class="tombolmasuk" type="button" onclick="document.getElementById('popup').style.display='block'">Tambah Task</button>
         </div>
+
+        <!-- Halaman Pop Up -->
+        <div class="popup" id="popup">
+            <!-- Semua Halaman Input -->
+            <div class="halamaninputpopup">
+                <h2 style="text-align: center; margin-bottom: 20px;">Task Baru</h2>
+                <form class="formpopup" action="homepage.php" method="post">
+                    <input class="inputpopup" type="text" name="title" id="" required placeholder="Judul Task">
+                    <input class="inputpopup" type="date" name="date" id="" required>
+
+                    <!-- Halaman Tombol di Pop up -->
+                    <div class="tombolpopup">
+                        <button id="tombolbatalpopup" class="tombolkeluar"
+                         type="button" onclick="document.getElementById('popup').style.display='none'">Batal</button>
+                         <button id="tombolmasukpopup" class="tombolmasuk" type="submit">Simpan</button>
+                    </div>
+
+                    <!-- Untuk Memuat Halaman Advance Task -->
+                    <a href="advance-task.php"
+                     style="display: block; text-align: center; margin-top: 20px; text-decoration: none; color: blue;"
+                     >Tambah Advance Task</a>
+                </form>
+            </div>
+        </div>
     </div>
 </body>
 </html>
