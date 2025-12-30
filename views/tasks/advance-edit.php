19e62a439cb6aa31f50a26a45d65bd14161674a1
nuryanaputra19-stack <nuryanaputra19@gmail.com>
2025-12-30 15:12:39 +0800

    menambahkan styling untuk halaman advance edit task

--- views/tasks/advance-edit.php
+++ views/tasks/advance-edit.php
@@ -4,6 +4,88 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Advance Edit Task</title>
+    <style>
+        *, html {
+            margin: 0px;
+            padding: 0px;
+            font-family: 'Segoe UI', sans-serif;
+        }
+
+        body {
+            background-color: #def1fbff;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            width: 50%;
+        }
+
+        .card {
+            background-color: white;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+            border-radius: 20px;
+            padding: 40px;
+            width: 400px;
+        }
+
+        .cardbody {
+            display: block;
+            justify-content: center;
+            align-items: center;
+            background-color: #F0FFFF;
+            border: 2px solid lightblue;
+            border-radius: 20px;
+            padding: 20px;
+        }
+
+        .inputtask {
+            display: block;
+            border: 2px solid #ddd;
+            border-radius: 10px;
+            padding: 10px;
+            margin-bottom: 20px;
+            width: 92%;
+        }
+
+        .tombol {
+            display: flex;
+            justify-content: end;
+            gap: 10px;
+            margin-top: 20px;
+        }
+
+        .tombolmasuk {
+            background-color: blue;
+            color: white;
+            border: none;
+            padding: 5px 20px;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        .tombolkeluar:hover {
+            background-color: darkred;
+        }
+
+        .tombolkeluar {
+            background-color: red;
+            color: white;
+            border: none;
+            padding: 5px 20px;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        .tombolmasuk:hover {
+            background-color: darkblue;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
