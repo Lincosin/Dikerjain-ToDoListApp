3e5c835648c9e213dfcb1a95cc091bbf643dead3
nuryanaputra19-stack <nuryanaputra19@gmail.com>
2025-12-30 12:55:49 +0800

    file untuk homepage

---
+++ views/tasks/homepage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Menu Samping jika isi -->
+    <aside>
+        <nav>
+
+        </nav>
+    </aside>
+
+    <!-- Halaman Utama -->
+    <div class="container">
+        <div class="header">
+            <!-- Judul -->
+            <header>
+                <h1>📝 To-Do List</h1>
+            </header>
+
+            <!-- Tombol buat Task Simple -->
+            <button class="tombolmasuk" type="button" onclick="document.getElementById('popup').style.display='block'">Tambah Task</button>
+        </div>
+    </div>
+</body>
+</html>
